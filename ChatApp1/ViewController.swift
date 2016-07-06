982eb36c59ed1934d09bfb213112b9d08fc305e1
Ralph Venuto <Ralphv1@ralphs-macbook-pro-2.local>
2016-07-06 15:30:19 -0400

    Initial Commit

---
+++ ChatApp1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatApp1
+//
+//  Created by Ralph Venuto on 7/6/16.
+//  Copyright © 2016 Ralph Venuto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
